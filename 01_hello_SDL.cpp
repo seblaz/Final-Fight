95c6517222066f8e30f123a040769bd8cd2713a7
sebas <sebastian.blazquez96@gmail.com>
2019-09-01 01:10:38 -0300

    Se implementa un game loop basico.

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -37,7 +37,7 @@ int main( int argc, char* args[] )
 
 			//Fill the surface white
 			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
+
 			//Update the surface
 			SDL_UpdateWindowSurface( window );
 
--- calc.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by sebas on 25/8/19.
-//
-#include <stdio.h>
-int main( int argc, char* args[] ) {
-    printf("Hello from calc!");
-}
-
