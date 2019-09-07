17ab7d4d373ff1eba06d27440c578b3aea3a7bda
felipe <felipegonzalez2112@gmail.com>
2019-09-07 18:33:58 -0300

    Modificamos inicializacion de las superficies

--- graficos/GraficoDePersonaje.cpp
+++ graficos/GraficoDePersonaje.cpp
@@ -16,18 +16,25 @@ void GraficosDePersonaje::actualizar(SDL_Renderer *renderer, Mapeable &mapeable)
     Posicion posicion = personaje.posicion();
     SDL_Rect posicionJugador = {posicion.getX(), posicion.getY(), 200, 400};
 
-    //Se carga la plantilla de sprites
-    // Se debe cargar la primera vez!!! TODO
+    //Se carga la plantilla de sprites si no esta cargada
 
-    SDL_Surface* spriteOriginal = IMG_Load("assets/personajes/coddy.png");
-    SDL_SetColorKey( spriteOriginal, SDL_TRUE, SDL_MapRGB( spriteOriginal->format, 0X58, 0xB8, 0xF8 ) );
-    SDL_Texture* spriteTransparente = SDL_CreateTextureFromSurface( renderer, spriteOriginal );
+    if (obtenerSprite() == nullptr){
+        printf("Entra solo 1 vez");
+        SDL_Surface* spriteOriginal = IMG_Load("assets/personajes/coddy.png");
+        setearSprite(spriteOriginal);
+        SDL_SetColorKey( spriteOriginal, SDL_TRUE, SDL_MapRGB( spriteOriginal->format, 0X58, 0xB8, 0xF8 ) );
+        SDL_Texture* spriteTransparente = SDL_CreateTextureFromSurface( renderer, spriteOriginal );
+        setearTextura(spriteTransparente);
+    }
 
     //Se setea la posicion de la plantilla de sprites (La posicion y dimension que se va a renderizar LA PLANTILLA DE SPRITES, no el hitbox)
     SDL_Rect posicionSprite = { 5, 0, 40, 100 };
 
-    //Se renderiza en la ventana la imagen, la posicion del sprite, y la posicion del jugador
-    SDL_RenderCopy(renderer, spriteTransparente, &posicionSprite, &posicionJugador);
+    //Se renderiza en la ventana la imagen
+    // , la posicion del sprite, y la posicion del jugador
+    SDL_RenderCopy(renderer, obtenerTextura(), &posicionSprite, &posicionJugador);
     SDL_RenderPresent(renderer);
 
+
 }
+
--- graficos/GraficoDePersonaje.h
+++ graficos/GraficoDePersonaje.h
@@ -12,10 +12,16 @@ class GraficosDePersonaje : public Grafico {
 
 private:
     SDL_Texture *texture = nullptr;
+    SDL_Surface *sprite = nullptr;
 
 public:
     void actualizar(SDL_Renderer *renderer, Mapeable &mapeable) override;
-    ~GraficosDePersonaje() override = default;;
+    ~GraficosDePersonaje() override = default;
+
+    void setearSprite(SDL_Surface* spriteNuevo) { sprite = spriteNuevo; }
+    SDL_Surface* obtenerSprite() const {return sprite;}
+    void setearTextura(SDL_Texture* textureNueva) { texture = textureNueva; }
+    SDL_Texture* obtenerTextura() const {return texture;}
 };
 
 
