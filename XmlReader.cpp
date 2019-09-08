abac06c92aa7d56096da61ee6edccb7828abd672
lpercivati <l.percivati@gmail.com>
2019-09-08 20:41:51 -0300

    Borrado de archivos viejos

--- XmlReader.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by leo on 3/9/19.
-//
-
-#include "XmlReader.h"
--- calc.cpp
+++ calc.cpp
@@ -2,7 +2,21 @@
 // Created by sebas on 25/8/19.
 //
 #include <stdio.h>
-int main( int argc, char* args[] ) {
-    printf("Hello from calc!");
-}
+#include "FilesManager/Logger.h"
+using namespace std;
 
+int main() {
+    FileManager* manager = new FileManager();
+
+    Logger::Instance()->logMessage("ERROR", "Hola buen dia");
+    /*Logger::Instance()->logMessage("ERROR", "Alto error");
+    Logger::Instance()->logMessage("DEBUG", "Esto es debug");
+    Logger::Instance()->logMessage("jajaj", "Alto error");
+
+    Logger::Instance()->setLevel("ERROR");
+    Logger::Instance()->logMessage("ERROR", "Este debe ir");
+    Logger::Instance()->logMessage("DEBUG", "Este no");
+
+    Logger::Destroy();*/
+    return 0;
+}
