9d525bfc2731e0c0ceea688dc9f56e77560fb005
sebas <sebastian.blazquez96@gmail.com>
2019-09-14 16:01:40 -0300

    Se divide los tests.

--- test/XmlReaderTest.cpp
+++ test/XmlReaderTest.cpp
@@ -16,11 +16,17 @@ TEST(XmlReader, AlPedirConfiguracionesQueExistenLasDevuelveComoString) {
 
     string level = reader.getValue("/debug/level");
     string cuchillos = reader.getValue("/escenario/objetos/cuchillo/cantidad");
-    string noValido = reader.getValue("/noValido/level");
-    string cuchillosNoValido = reader.getValue("/escenario/objetos/cuchillo/cantidad2");
 
     EXPECT_EQ(level, "DEBUG");
     EXPECT_EQ(cuchillos, "5");
+}
+
+TEST(XmlReader, AlPedirConfiguracionesQueNoExistenDevuelveUnaCadenaVacia) {
+    XmlReader reader = XmlReader("s");
+
+    string noValido = reader.getValue("/noValido/level");
+    string cuchillosNoValido = reader.getValue("/escenario/objetos/cuchillo/cantidad2");
+
     EXPECT_EQ(noValido, "");
     EXPECT_EQ(cuchillosNoValido, "");
 }
