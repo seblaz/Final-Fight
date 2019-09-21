5237359b6886caeb1a8b7d154daf870ab561589f
sebas <sebastian.blazquez96@gmail.com>
2019-09-21 19:38:01 -0300

    Se plantean los estados y comportamientos.

--- graficos/Grafico.cpp
+++ graficos/Grafico.cpp
@@ -1,8 +1,15 @@
-#include <SDL_rect.h>
-#include "../modelo/Posicion.h"
-#include "../servicios/Locator.h"
+//
+// Created by sebas on 21/9/19.
+//
 
-SDL_Rect calcularPosicionEnPantalla(Posicion posicionEnMapa, SDL_Rect posicionEnSprite, float esacalaDeAnimacion){
+#include <SDL_rect.h>
+#include "Grafico.h"
+#include "../servicios/Locator.h"
+#include "../modelo/Posicion.h"
+#include "Animacion.h"
+#include "sprite/Sprite.h"
+
+SDL_Rect calcularPosicionEnPantalla2(Posicion posicionEnMapa, SDL_Rect posicionEnSprite, float esacalaDeAnimacion){
     Configuracion *config = Locator::configuracion();
 
     float escala = config->getFloatValue("/escala/mapeables") * esacalaDeAnimacion;
@@ -22,4 +29,17 @@ SDL_Rect calcularPosicionEnPantalla(Posicion posicionEnMapa, SDL_Rect posicionEn
                                 int(posicionEnSprite.h * escala)};
 
     return posicionEnPantalla;
+}
+
+void Grafico::actualizar(Entidad *entidad) {
+    SDL_Renderer *renderer = Locator::renderer();
+    auto* posicion = entidad->getEstado<Posicion>();
+    auto *animacion = entidad->getEstado<Animacion>();
+    auto *sprite = entidad->getEstado<Sprite>();
+//    Posicion nuevaPosicion(posicion->getX() - fisicaDeEscenario.posicion(), fisica->posicion().getY(),
+//                           fisica->posicion().getZ());
+    SDL_Rect posicionEnSprite = animacion->actualizarYDevolverPosicion();
+    SDL_Rect posicionEnPantalla = calcularPosicionEnPantalla2(*posicion, posicionEnSprite, animacion->escala());
+
+    SDL_RenderCopy(renderer, sprite->getTexture(), &posicionEnSprite, &posicionEnPantalla);
 }
