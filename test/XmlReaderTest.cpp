6ed2490b77651c474e1aeb1bb0c7e650d7fd6109
sebas <sebastian.blazquez96@gmail.com>
2019-09-24 23:37:09 -0300

    Se arreglan los tests.

--- test/XmlReaderTest.cpp
+++ test/XmlReaderTest.cpp
@@ -9,32 +9,18 @@ TEST(XmlReader, AlPedirConfiguracionesQueExistenLasDevuelveComoString) {
     Configuracion reader = Configuracion();
 
     string level = reader.getValue("/debug/level");
-    string cuchillos = reader.getValue("/escenario/objetos/cuchillo/cantidad");
+    string fps = reader.getValue("/fps");
+
+    EXPECT_EQ(level, "DEBUG");
+    EXPECT_EQ(fps, "60");
+}
+
+TEST(XmlReader, AlPedirConfiguracionesQueNoExistenDevuelveUnaCadenaVacia) {
+    Configuracion reader = Configuracion();
+
     string noValido = reader.getValue("/noValido/level");
     string cuchillosNoValido = reader.getValue("/escenario/objetos/cuchillo/cantidad2");
 
-    EXPECT_EQ(level, "DEBUG");
-    EXPECT_EQ(cuchillos, "5");
     EXPECT_EQ(noValido, "");
     EXPECT_EQ(cuchillosNoValido, "");
 }
-
-//TEST(XmlReader, PediConfiguracionesPathNoValido) {
-//    Configuracion reader = Configuracion("/Novalido/");
-//
-//    string level = reader.getValue("/debug/level");
-//    string cuchillos = reader.getValue("/escenario/objetos/cuchillo/cantidad");
-//
-//    EXPECT_EQ(level, "DEBUG");
-//    EXPECT_EQ(cuchillos, "5");
-//}
-
-//TEST(XmlReader, AlPedirConfiguracionesQueNoExistenDevuelveUnaCadenaVacia) {
-//    Configuracion reader = Configuracion("s");
-//
-//    string noValido = reader.getValue("/noValido/level");
-//    string cuchillosNoValido = reader.getValue("/escenario/objetos/cuchillo/cantidad2");
-//
-//    EXPECT_EQ(noValido, "");
-//    EXPECT_EQ(cuchillosNoValido, "");
-//}
