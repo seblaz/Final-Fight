9767ad729a534e09ee37cb46244646aba200d5b6
sebas <sebastian.blazquez96@gmail.com>
2019-09-25 15:39:43 -0300

    Se agrega fondo al no existir el sprite.

--- graficos/Sprite.cpp
+++ graficos/Sprite.cpp
@@ -7,19 +7,25 @@
 
 Sprite::Sprite(SDL_Renderer *renderer, const string &rutaSprite) :
         renderer(renderer) {
-    Logger* logger = Locator::logger();
+    Logger *logger = Locator::logger();
     ifstream infile(rutaSprite);
     if (!infile.good()) {
         logger->log(DEBUG, "No existe el sprite:" + rutaSprite + ". Se va a cargar una textura por defecto.");
 
-        TTF_Font *Sans = TTF_OpenFont("assets/fuentes/open-sans/OpenSans-Regular.ttf", 64);
-        if (Sans == nullptr)
+        TTF_Font *Sans = TTF_OpenFont("assets/fuentes/open-sans/OpenSans-Regular.ttf", 24);
+        if (Sans == nullptr) {
             logger->log(ERROR, string("No se pudo cargar la fuente. El error fue: ").append(TTF_GetError()));
-
-        SDL_Color black = {0, 0,0};
-        SDL_Surface *surfaceMessage = TTF_RenderText_Solid(Sans, "No existe el sprite. No existe el sprite. No existe el sprite. No existe el sprite.", black);
-
-        texture = SDL_CreateTextureFromSurface(renderer, surfaceMessage); //now you can convert it into a texture
+            SDL_Surface *surfaceColor = SDL_CreateRGBSurface(0, 2000, 2000,32, 0, 0, 0, 0);
+            SDL_FillRect(surfaceColor, nullptr, SDL_MapRGB(surfaceColor->format, rand(), rand(), rand()));
+            texture = SDL_CreateTextureFromSurface(renderer, surfaceColor); //now you can convert it into a texture
+        } else {
+            SDL_Color black = {0, 0, 0};
+            SDL_Surface *surfaceMessage = TTF_RenderText_Solid(Sans, "No existe el sprite. No existe el sprite. No existe el sprite. No existe el sprite.", black);
+            SDL_Surface *surfaceColor = SDL_CreateRGBSurface(0, 2000, 2000,32, 0, 0, 0, 0);
+            SDL_FillRect(surfaceColor, nullptr, SDL_MapRGB(surfaceColor->format, rand(), rand(), rand()));
+            SDL_BlitSurface(surfaceMessage, nullptr, surfaceColor, nullptr);
+            texture = SDL_CreateTextureFromSurface(renderer, surfaceColor); //now you can convert it into a texture
+        }
         ancho_ = 1000;
 
     } else {
