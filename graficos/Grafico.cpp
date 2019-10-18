4d59453b7ff4251bfc0626d91e06d35a53d9c6d9
felipe <felipegonzalez2112@gmail.com>
2019-10-17 21:35:26 -0300

    Dibujamos un triangulo

--- graficos/Grafico.cpp
+++ graficos/Grafico.cpp
@@ -10,6 +10,7 @@
 #include "Sprite.h"
 #include "../modelo/Orientacion.h"
 #include "../modelo/Actividad.h"
+#include "../modelo/Personaje.h"
 
 SDL_Rect calcularPosicionEnPantalla(Posicion posicionEnMapa, SDL_Rect posicionEnSprite, float esacalaDeAnimacion) {
     Configuracion *config = Locator::configuracion();
@@ -44,6 +45,9 @@ void Grafico::actualizar(Entidad *entidad) {
         auto *orientacion = entidad->getEstado<Orientacion>("orientacion");
         auto *actividad = entidad->getEstado<Actividad>("actividad");
 
+        auto *personaje = entidad->getEstado<Personaje>("personaje");
+
+
         Posicion nuevaPosicion(posicion->getX() - posicionDeEscenarioX, posicion->getY(), posicion->getZ());
         SDL_Rect posicionEnSprite = animacion->actualizarYDevolverPosicion();
         SDL_Rect posicionEnPantalla = calcularPosicionEnPantalla(nuevaPosicion, posicionEnSprite, animacion->escala());
@@ -54,6 +58,29 @@ void Grafico::actualizar(Entidad *entidad) {
             } else {
                 SDL_SetTextureColorMod(sprite->getTexture(), 255, 255, 255);
             }
+        }
+
+        if (personaje != nullptr){
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+
+       //     SDL_RenderSetScale( renderer, 2.0, 2.0 );
+
+            SDL_RenderDrawLine(renderer, nuevaPosicion.getX() , -nuevaPosicion.getY() + 380 *2,nuevaPosicion.getX()+40 *2 ,-nuevaPosicion.getY() + 380 *2);
+            SDL_RenderDrawLine(renderer, nuevaPosicion.getX() + 20, -nuevaPosicion.getY() + 420 *2,nuevaPosicion.getX()+40 *2 ,-nuevaPosicion.getY() + 380 *2);
+            SDL_RenderDrawLine(renderer, nuevaPosicion.getX() , -nuevaPosicion.getY() + 380 *2,nuevaPosicion.getX()+20 *2 ,-nuevaPosicion.getY() + 420 *2);
+
+         //   SDL_RenderSetScale(renderer,
+           //                          1,
+             //                        1);
+
+  //            SDL_RenderDrawLine(renderer, nuevaPosicion.getX() , -nuevaPosicion.getY() + 300,nuevaPosicion.getX()+30 ,-nuevaPosicion.getY() + 320);
+  //          SDL_RenderDrawLine(renderer, nuevaPosicion.getX() , -nuevaPosicion.getY() + 300,nuevaPosicion.getX()+30 ,-nuevaPosicion.getY() + 320);
+  //          SDL_RenderDrawLine(renderer, posicion->getX()+300, posicion->getY()+240, nuevaPosicion.getX()+340, nuevaPosicion.getY()+240);
+  //          SDL_RenderDrawLine(renderer, posicion->getX()+340, posicion->getY()+240, nuevaPosicion.getX()+320, nuevaPosicion.getY()+200);
+
+  //          SDL_RenderDrawLine(renderer, posicion->getX() + 300, posicion->getY() + 200, nuevaPosicion.getX(),  nuevaPosicion.getY() + 200);
+          //  SDL_RenderDrawLine(renderer, posicion->getX()+300, posicion->getY()+240, nuevaPosicion.getX()+340, nuevaPosicion.getY()+240);
+          //  SDL_RenderDrawLine(renderer, posicion->getX()+340, posicion->getY()+200, nuevaPosicion.getX()+320, nuevaPosicion.getY()+240);
         }
 
         if ((orientacion != nullptr) && (!orientacion->adelante)) {
