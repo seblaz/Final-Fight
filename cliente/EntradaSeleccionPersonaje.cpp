f2458731308ea1ee15425dacc860c08dbb542b41
felipe <felipegonzalez2112@gmail.com>
2019-10-20 20:09:10 -0300

    Pequeño refactor con la velocidad del menu

--- cliente/EntradaSeleccionPersonaje.cpp
+++ cliente/EntradaSeleccionPersonaje.cpp
@@ -14,13 +14,11 @@ void EntradaSeleccionPersonaje::actualizar(Entidad *pantalla) {
         if (activo) {
             const Uint8 *entrada = SDL_GetKeyboardState(nullptr);
             if (entrada[SDL_SCANCODE_LEFT]) {
-                Locator::logger()->log(DEBUG, "SE selecciono izquierda");
                 cambiarAlPersonajeAnterior(pantalla, personajeMarcado);
             } else if (entrada[SDL_SCANCODE_RIGHT]) {
-                Locator::logger()->log(DEBUG, "Se selecciono derecha");
                 cambiarAlPersonajeSiguiente(pantalla, personajeMarcado);
             } else if (entrada[SDL_SCANCODE_RETURN]) {
-                Locator::logger()->log(DEBUG, "Se selecciono enter. Borrar indicador");
+                Locator::logger()->log(DEBUG, "Se selecciono enter");
                 activo = false;
             }
             framesInactivo = framesPorAccion;
@@ -33,22 +31,22 @@ void EntradaSeleccionPersonaje::actualizar(Entidad *pantalla) {
 void EntradaSeleccionPersonaje::cambiarAlPersonajeSiguiente(Entidad *pantalla, Personaje *personajeMarcado) {
     switch (personajeMarcado->getPersonaje()) {
         case GUY: {
-            Locator::logger()->log(DEBUG, "Se marco a cody");
+            Locator::logger()->log(INFO, "Se marco a cody");
             pantalla->agregarEstado("personajeMarcado", new Personaje(CODY));
         }
             break;
         case CODY: {
-            Locator::logger()->log(DEBUG, "Se marco a haggar");
+            Locator::logger()->log(INFO, "Se marco a haggar");
             pantalla->agregarEstado("personajeMarcado", new Personaje(HAGGAR));
         }
             break;
         case HAGGAR: {
-            Locator::logger()->log(DEBUG, "Se marco a maki");
+            Locator::logger()->log(INFO, "Se marco a maki");
             pantalla->agregarEstado("personajeMarcado", new Personaje(MAKI));
         }
             break;
         case MAKI: {
-            Locator::logger()->log(DEBUG, "Se marco a guy");
+            Locator::logger()->log(INFO, "Se marco a guy");
             pantalla->agregarEstado("personajeMarcado", new Personaje(GUY));
         }
             break;
@@ -58,22 +56,22 @@ void EntradaSeleccionPersonaje::cambiarAlPersonajeSiguiente(Entidad *pantalla, P
 void EntradaSeleccionPersonaje::cambiarAlPersonajeAnterior(Entidad *pantalla, Personaje *personajeMarcado) {
     switch (personajeMarcado->getPersonaje()) {
         case GUY: {
-            Locator::logger()->log(DEBUG, "Se marco a maki");
+            Locator::logger()->log(INFO, "Se marco a maki");
             pantalla->agregarEstado("personajeMarcado", new Personaje(MAKI));
         }
             break;
         case CODY: {
-            Locator::logger()->log(DEBUG, "Se marco a guy");
+            Locator::logger()->log(INFO, "Se marco a guy");
             pantalla->agregarEstado("personajeMarcado", new Personaje(GUY));
         }
             break;
         case HAGGAR: {
-            Locator::logger()->log(DEBUG, "Se marco a cody");
+            Locator::logger()->log(INFO, "Se marco a cody");
             pantalla->agregarEstado("personajeMarcado", new Personaje(CODY));
         }
             break;
         case MAKI: {
-            Locator::logger()->log(DEBUG, "Se marco a haggar");
+            Locator::logger()->log(INFO, "Se marco a haggar");
             pantalla->agregarEstado("personajeMarcado", new Personaje(HAGGAR));
         }
             break;
--- cliente/EntradaSeleccionPersonaje.h
+++ cliente/EntradaSeleccionPersonaje.h
@@ -5,19 +5,15 @@
 #ifndef FINAL_FIGHT_ENTRADASELECCIONPERSONAJE_H
 #define FINAL_FIGHT_ENTRADASELECCIONPERSONAJE_H
 
-#include <SDL_system.h>
 #include "../modelo/Entidad.h"
 #include "../servicios/Configuracion.h"
 #include "../modelo/Personaje.h"
 
 class EntradaSeleccionPersonaje : public Comportamiento {
 
-private:
-    int delay = 10;
-
 public:
 
-    int framesPorAccion = 20;
+    int framesPorAccion = 7;
     int framesInactivo = 0;
 
     bool activo = true;
