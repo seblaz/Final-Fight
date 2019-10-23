12f6311e91097d96157cadd395d90c0505b21925
fmendiola <mendiola.f92@hotmail.com>
2019-10-23 02:50:24 -0300

    Revert "Modifico animaciones personajes"
    This reverts commit 16400941

--- graficos/animaciones/FabricaDeAnimacionesDeGuy.cpp
+++ graficos/animaciones/FabricaDeAnimacionesDeGuy.cpp
@@ -1,21 +1,21 @@
 #include "FabricaDeAnimacionesDeGuy.h"
 
-Animacion* FabricaDeAnimacionesDeGuy::caminando() {
+AnimacionCliente* FabricaDeAnimacionesDeGuy::caminando() {
     vector<SDL_Rect> posiciones = {
-            {5, 100, 35, 95},
-            {40, 100, 45, 95},
-            {85, 100, 45, 95},
-            {125, 100, 45, 95},
-            {170, 100, 45, 95},
-            {220, 100, 35, 95},
+            {5, 105, 35, 90},
+            {40, 105, 45, 90},
+            {90, 105, 42, 90},
+            {130, 105, 42, 90},
+            {172, 105, 42, 90},
+
     };
 
-    vector<float> duraciones = {1, 1, 1, 1, 1, 1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+//    vector<float> duraciones = {1, 1, 1, 1, 1};
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion* FabricaDeAnimacionesDeGuy::saltando() {
+AnimacionCliente* FabricaDeAnimacionesDeGuy::saltando() {
     vector<SDL_Rect> posiciones = {
             {5, 205, 50, 90},
             {55, 195, 50, 100},
@@ -24,47 +24,38 @@ Animacion* FabricaDeAnimacionesDeGuy::saltando() {
     };
 
     vector<float> duraciones = {1, 1, 1, 1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion* FabricaDeAnimacionesDeGuy::reposando() {
+AnimacionCliente* FabricaDeAnimacionesDeGuy::reposando() {
     vector<SDL_Rect> posiciones = {{5, 0, 55, 95}};
-    vector<float> duraciones = {1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+//    vector<float> duraciones = {1};
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion *FabricaDeAnimacionesDeGuy::agachado() {
+AnimacionCliente *FabricaDeAnimacionesDeGuy::agachado() {
     vector<SDL_Rect> posiciones = {{343, 670, 50, 90}};
-    vector<float> duraciones = {1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+//    vector<float> duraciones = {1};
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion* FabricaDeAnimacionesDeGuy::golpear() {
+AnimacionCliente* FabricaDeAnimacionesDeGuy::golpear() {
     vector<SDL_Rect> posiciones = {
             //{6, 380, 51, 100},
-            //{5, 0, 55, 95},
-            //{60, 380, 65, 100},
-            //{122, 380, 63, 100},
-            {5, 205, 50, 90},
+            {60, 380, 65, 100},
+            {122, 380, 63, 100},
             {185, 380, 70, 100},
-            //{260, 380, 70, 100},
-            //{330, 380, 70, 100},
-            /*{395, 380, 60, 100},
-            {448, 380, 50, 100},
-            {497, 380, 50, 100},
-            {540, 370, 58, 105},
-            {600, 380, 58, 100},*/
     };
 
-    vector<float> duraciones = {1, 2, 1};
-
-    return new Animacion(posiciones, duraciones, 16, 4.5);
+//    vector<float> duraciones = {1, 2, 1};
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 16, 4.5);
 }
 
-Animacion *FabricaDeAnimacionesDeGuy::saltandoAdelante() {
+AnimacionCliente *FabricaDeAnimacionesDeGuy::saltandoAdelante() {
     vector<SDL_Rect> posiciones = {
             {5, 195, 50, 90},
             {55, 195, 50, 100},
@@ -76,17 +67,17 @@ Animacion *FabricaDeAnimacionesDeGuy::saltandoAdelante() {
     };
 
     vector<float> duraciones = {1, 1, 1, 1, 1, 1, 1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion *FabricaDeAnimacionesDeGuy::patadaBasica() {
+AnimacionCliente *FabricaDeAnimacionesDeGuy::patadaBasica() {
     vector<SDL_Rect> posiciones = {
             //{155, 500, 45, 80},
             {200, 500, 85, 80},
     };
 
-    vector<float> duraciones = {1, 1, 3, 2};
-
-    return new Animacion(posiciones, duraciones, 35, 4.5);
+//    vector<float> duraciones = {1, 1, 3, 2};
+    return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 35, 4.5);
 }
--- graficos/animaciones/FabricaDeAnimacionesDeHaggar.cpp
+++ graficos/animaciones/FabricaDeAnimacionesDeHaggar.cpp
@@ -1,6 +1,6 @@
 #include "FabricaDeAnimacionesDeHaggar.h"
 
-Animacion* FabricaDeAnimacionesDeHaggar::caminando() {
+AnimacionCliente* FabricaDeAnimacionesDeHaggar::caminando() {
     vector<SDL_Rect> posiciones = {
             {0,  103, 60, 104},
             {65,  103, 60, 104},
@@ -10,12 +10,12 @@ Animacion* FabricaDeAnimacionesDeHaggar::caminando() {
             {340, 100, 60, 104},
     };
 
-    vector<float> duraciones = {1, 1, 1, 1, 1, 1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    //vector<float> duraciones = {1, 1, 1, 1, 1, 1};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion* FabricaDeAnimacionesDeHaggar::saltando() {
+AnimacionCliente* FabricaDeAnimacionesDeHaggar::saltando() {
     vector<SDL_Rect> posiciones = {
             {5,  212, 60, 104},
             {65,  212, 60, 104},
@@ -24,57 +24,56 @@ Animacion* FabricaDeAnimacionesDeHaggar::saltando() {
             {5,  212, 60, 104},
             };
 
-    vector<float> duraciones = {1, 1, 1, 1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    //vector<float> duraciones = {1, 1, 1, 1};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion* FabricaDeAnimacionesDeHaggar::reposando() {
+AnimacionCliente* FabricaDeAnimacionesDeHaggar::reposando() {
     vector<SDL_Rect> posiciones = {{0, 0, 64, 104}};
-    vector<float> duraciones = {1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    //vector<float> duraciones = {1};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion *FabricaDeAnimacionesDeHaggar::agachado() {
+AnimacionCliente *FabricaDeAnimacionesDeHaggar::agachado() {
     vector<SDL_Rect> posiciones = {{360, 825, 60, 104}};
-    vector<float> duraciones = {1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    //vector<float> duraciones = {1};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion* FabricaDeAnimacionesDeHaggar::golpear() {
+AnimacionCliente* FabricaDeAnimacionesDeHaggar::golpear() {
     vector<SDL_Rect> posiciones = {
             {10, 440, 48, 90},
             //{58, 440, 65, 90},
             {113, 440, 97, 90},
             //{203, 437, 60, 95},
-            //{260, 440, 60, 95},
-            //{320, 440, 85, 90},
-
+            {260, 440, 60, 95},
+            {320, 440, 85, 90},
     };
 
-    vector<float> duraciones = {1, 2, 1};
-
-    return new Animacion(posiciones, duraciones, 16, 4.5);
+    //vector<float> duraciones = {1, 2, 1};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 16, 4.5);
 }
 
-Animacion *FabricaDeAnimacionesDeHaggar::saltandoAdelante() {
+AnimacionCliente *FabricaDeAnimacionesDeHaggar::saltandoAdelante() {
     vector<SDL_Rect> posiciones = {
             {5,  212, 60, 104},
     };
 
-    vector<float> duraciones = {1, 1, 1, 1, 1, 1, 1};
-
-    return new Animacion(posiciones, duraciones, 60, 4.5);
+    //vector<float> duraciones = {1, 1, 1, 1, 1, 1, 1};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 60, 4.5);
 }
 
-Animacion *FabricaDeAnimacionesDeHaggar::patadaBasica() {
+AnimacionCliente *FabricaDeAnimacionesDeHaggar::patadaBasica() {
     vector<SDL_Rect> posiciones = {
             {195, 540, 90, 65},
     };
 
-    vector<float> duraciones = {1, 1, 3, 2};
-
-    return new Animacion(posiciones, duraciones, 35, 4.5);
+    //vector<float> duraciones = {1, 1, 3, 2};
+        return new AnimacionCliente(posiciones, 4.5);
+    //return new AnimacionCliente(posiciones, duraciones, 35, 4.5);
 }
