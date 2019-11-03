8f1af910149f2660d56f8fc8d4bee87b87e4a0f3
lpercivati <l.percivati@gmail.com>
2019-11-03 18:58:14 -0300

    Modo test

---
+++ eventos/EventoModoTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leo on 3/11/19.
+//
+
+#include "EventoModoTest.h"
---
+++ eventos/EventoModoTest.h
@@ -0,0 +1,14 @@
+//
+// Created by leo on 3/11/19.
+//
+
+#ifndef FINAL_FIGHT_EVENTOMODOTEST_H
+#define FINAL_FIGHT_EVENTOMODOTEST_H
+
+
+class EventoModoTest {
+
+};
+
+
+#endif //FINAL_FIGHT_EVENTOMODOTEST_H
