3e1622246a2c81e7064840ddaccd792b4cd9d053
Leo <l.percivati@gmail.com>
2019-11-13 09:16:44 -0300

    Primeros cambios para recibir golpe

---
+++ modelo/serializables/Arma.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leo on 12/11/19.
+//
+
+#include "Arma.h"
---
+++ modelo/serializables/Arma.h
@@ -0,0 +1,14 @@
+//
+// Created by leo on 12/11/19.
+//
+
+#ifndef FINAL_FIGHT_ARMA_H
+#define FINAL_FIGHT_ARMA_H
+
+
+class Arma {
+
+};
+
+
+#endif //FINAL_FIGHT_ARMA_H
